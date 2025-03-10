e269c861acbdaffc6bf27c78f53daf8f57a90d41
ilikemysuitcase365 <karbaaa1234@gmail.com>
2025-03-10 17:45:19 +0800

    COMMIT

---
+++ act1.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number= 40
+sum = mean1*total_number
+print ("the sum of 40 numbers:",sum)
+sum2 = sum - wrong_number + correct_number
+print("the corrected sum is:",sum2)
+mean2 = sum2/total_number
+print("the correct mean", mean2)
---
+++ act2.py
@@ -0,0 +1,12 @@
+a = 12
+b = 2
+c = 1 
+d = 4
+answer = b**c+d/b*c+a
+print("answer",answer)
+n1 = int (input("enter the numerator:"))
+n2 =int (input("enter the denominator:"))
+if n1%n2 == 0:
+    print(str(n1)+" is divisble by "+str(n2))
+else:
+    print(str(n1)+"is not divisble by"+str(n2))
